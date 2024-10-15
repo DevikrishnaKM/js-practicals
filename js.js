62b21e3f249ea7219d46e41d606a6fc0e4bf869f
Devi krishna <devikrishna2345@gmail.com>
2024-10-15 19:45:33 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,48 @@
+// 1)ispalindrome without built in
+
+function palindrome(array){
+    let len=array.length
+    for(let i=0;i<len/2;i++){
+        if(array[i]!==array[len-1-i])return false
+    }
+    return true
+}
+console.log(palindrome("racecar")); // true
+console.log(palindrome("hello"));   // false
+
+// 2)snake case to pascal
+function snakeToPascal(str){
+    let result=""
+    let capitalize=true
+    
+    for(let i=0;i<str.length;i++){
+        if(str[i]==="_"){
+            capitalize=true
+        }else{
+            if(capitalize){
+                result+=str[i].toUpperCase()
+                capitalize=false
+            }else{
+                result+=str[i]
+            }
+        }
+    }
+   return result
+}
+console.log(snakeToPascal('hello_world_example')); // HelloWorldExample
+console.log(snakeToPascal('convert_this_string')); // ConvertThisString
+
+// 3)flip negative from a mixed array
+
+function flipNegative(arr){
+    for(let i=0;i<arr.length;i++){
+        if(typeof arr[i]===Number && arr[i]<0){
+            arr[i]=-arr[i]
+        }
+    }
+    return arr
+}
+let mixedArray = [12, '-3', -5, 'hello', 42, -100, '100'];
+console.log(flipNegative(mixedArray));
+
+4)
